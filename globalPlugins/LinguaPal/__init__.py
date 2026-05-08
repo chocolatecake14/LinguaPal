746a2e8f51a67540f44acb76a9455a82f97e4bf1
Ali <exclusiveinfolab@gmail.com>
2026-05-08 18:32:42 +0500

    Fixed some more stuff and made the add-on compatible for NVDA's latest version (2026.1).

--- globalPlugins/LinguaPal/__init__.py
+++ globalPlugins/LinguaPal/__init__.py
@@ -16,7 +16,7 @@ import threading
 
 addonHandler.initTranslation()
 
-ADDON_VERSION = "0.1.4"
+ADDON_VERSION = "0.1.5"
 UPDATE_CHECK_URL = "https://baddobaddi.ddns.net/linguapal/update.json" 
 roleSECTION = "LinguaPal"
 
@@ -38,7 +38,7 @@ def sendGeminiSinglePrompt(promptText: str):
     data = {"contents": [{"role": "user", "parts": [{"text": promptText}]}]}
     try:
         response = requests.post(
-            f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key={apiGemini}",
+            f"https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite-preview:generateContent?key={apiGemini}",
             headers=headers, json=data)
         r = response.json()
         if response.status_code != 200:
@@ -64,7 +64,7 @@ def sendGeminiChat(messages):
     data = {"contents": gemini_messages}
     try:
         response = requests.post(
-            f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key={apiGemini}",
+            f"https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite-preview:generateContent?key={apiGemini}",
             headers=headers, json=data)
         r = response.json()
         if response.status_code != 200:
